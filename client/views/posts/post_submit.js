5d54609ea80203c4852680c347f39c95d26ede28
Brandon Nutter <brandon.nutter@gmail.com>
2014-08-22 19:16:44 -0700

    Added v10.1

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -7,8 +7,14 @@ Template.postSubmit.events({
       message: $(event.target).find('[name=message]').val()
     }
     Meteor.call('post', post, function(error, id) {
-      if (error)
-        return alert(error.reason);
+      if (error) {
+        // display the error to the user
+        throwError(error.reason);
+        if (error.error === 302)
+          Router.go('postPage', {_id: error.details})
+      } else {
+        Router.go('postPage', {_id: id});
+      }
     });
     Router.go('postsList');
   }
